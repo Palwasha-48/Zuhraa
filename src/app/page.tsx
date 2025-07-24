3ba0f09a0ed6e62bbdc70326b6f4514023add70b
Palwasheyyy <163187583+Palwasha-48@users.noreply.github.com>
2025-07-25 00:27:06 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { motion } from "framer-motion";
 import Hero from "@/components/Hero";
 import Navbar from "../components/Navbar";
 import Footer from "@/components/Footer";
