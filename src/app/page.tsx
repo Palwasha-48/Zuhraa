188bb8a8d1bc5c26b4d9afab8da42eeacf6e01c3
Palwasheyyy <163187583+Palwasha-48@users.noreply.github.com>
2025-08-06 17:42:07 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import Hero from "@/components/Hero";
-import Navbar from "../components/Navbar";
-import Footer from "@/components/Footer";
-
 export default function Home() {
   return (
     <>
