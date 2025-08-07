96c9edbc58c496fa2f0308d5142a4c6cc1f7e1dd
Palwasheyyy <163187583+Palwasha-48@users.noreply.github.com>
2025-08-07 17:45:17 +0500

    Update page.tsx

--- src/app/Asmaulhusna/page.tsx
+++ src/app/Asmaulhusna/page.tsx
@@ -8,20 +8,25 @@ export default function AsmaPage() {
 
   return (
     <div className="min-h-screen pt-32 px-6 py-10 bg-[#b2d1ba] text-[#1B4332]">
-
       {language === "en" ? <EnglishIntro /> : <UrduIntro />}
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 lg:grid-cols-9 gap-6 max-w-7xl mx-auto">
+      <div className="grid font-urdu grid-cols-2 sm:grid-cols-3 md:grid-cols-6 lg:grid-cols-9 gap-6 max-w-7xl mx-auto">
         {Array.isArray(names) &&
           names.map((name) => (
             <Link key={name.id} href={`/Asmaulhusna/${name.id}`}>
-              <div className="p-6 bg-[#ccd3b8] rounded-xl shadow-md hover:shadow-lg hover:scale-105 transform transition duration-200 cursor-pointer text-center">
-                <span className="text-3xl font-bold text-[#1B4332]">
+              <div className="p-6 h-35 bg-[#ccd3b8] rounded-xl shadow-md hover:shadow-lg hover:scale-105 transform transition duration-200 cursor-pointer text-center flex flex-col justify-center items-center">
+                <span
+                  className={`font-bold text-[#1B4332] block ${
+                    name.arabic.length > 15
+                      ? "text-xl leading-snug"
+                      : "text-3xl"
+                  }`}
+                >
                   {name.arabic}
                 </span>
                 <span className="block mt-2 text-lg font-semibold text-[#1B4332]">
                   {name.id}
-                  </span>
+                </span>
               </div>
             </Link>
           ))}
@@ -37,10 +42,10 @@ function EnglishIntro() {
         Asma-ul-Husna
       </h1>
       <p className="text-lg text-center text-[#2D6A4F] max-w-3xl mx-auto mb-10 leading-relaxed">
-        Asma‑ul‑Husna are the 99 beautiful names of Allah Ta’ala, each reflecting
-        His greatness and attributes. Understanding them and calling upon Allah
-        through these names is a form of worship. The Qur’an and Hadith mention
-        the virtues and blessings of these names.
+        Asma‑ul‑Husna are the 99 beautiful names of Allah Ta’ala, each
+        reflecting His greatness and attributes. Understanding them and calling
+        upon Allah through these names is a form of worship. The Qur’an and
+        Hadith mention the virtues and blessings of these names.
       </p>
     </>
   );
@@ -49,14 +54,18 @@ function EnglishIntro() {
 function UrduIntro() {
   return (
     <>
-    <h1 className="text-center mb-4 tracking-wide font-urdu text-5xl">
-      اسماء الحسنیٰ
-    </h1>
-    <p className="text-lg leadding-[2.5rem] font-urdu text-center text-[#2D6A4F] max-w-3xl mx-auto mb-10 leading-relaxed" dir="rtl">
-      اسماء الحسنیٰ اللہ تعالیٰ کے 99 خوبصورت نام ہیں، جو اس کی عظمت اور صفات
-      کو ظاہر کرتے ہیں۔ انہیں سمجھنا اور ان ناموں کے ذریعے اللہ کو پکارنا عبادت
-      کا حصہ ہے۔ قرآن اور حدیث میں ان ناموں کی فضیلت اور برکت کا ذکر آیا ہے۔
-    </p>
+      <h1 className="text-center mb-4 tracking-wide font-urdu text-5xl">
+        اسماء الحسنیٰ
+      </h1>
+      <p
+        className="text-lg leadding-[2.5rem] font-urdu text-center text-[#2D6A4F] max-w-3xl mx-auto mb-10 leading-relaxed"
+        dir="rtl"
+      >
+        اسماء الحسنیٰ اللہ تعالیٰ کے 99 خوبصورت نام ہیں، جو اس کی عظمت اور صفات
+        کو ظاہر کرتے ہیں۔ انہیں سمجھنا اور ان ناموں کے ذریعے اللہ کو پکارنا
+        عبادت کا حصہ ہے۔ قرآن اور حدیث میں ان ناموں کی فضیلت اور برکت کا ذکر آیا
+        ہے۔
+      </p>
     </>
   );
 }
